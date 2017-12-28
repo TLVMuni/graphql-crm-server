72eb30a79d11f6d18518d5b0deefd8694535e228
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-28 18:12:36 +0200

    Create App.test.js

---
+++ test/App.test.js
@@ -0,0 +1,13 @@
+import 'raf/polyfill';
+
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+
+//import App from '../App';
+
+test('Test App component', () => {
+
+  // const component = renderer.create(<App />);
+  // let tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+});
