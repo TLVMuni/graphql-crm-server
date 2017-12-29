4989de15e5ef5d34ab01709f8681161152bcb4f6
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-30 00:25:23 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -4,7 +4,7 @@ var express = require('express');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello Express");
 
 });
 
