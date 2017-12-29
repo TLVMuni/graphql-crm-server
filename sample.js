17110ad078c52d3fda22b8282e6c86f54f0d1b63
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-30 00:51:34 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -5,14 +5,19 @@ var cors = require('cors');
 var app = express();
 app.use('*', cors({ origin: '*' }));
 
-var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Express2");
-
-});
+// var server = http.createServer(function(request, response) {
+//
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello Express2");
+//
+// });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+// server.listen(port);
+//
+// console.log("Server running at http://localhost:%d", port);
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => {
+    console.log('GraphQL Server is listening on: ' + port);
+});
