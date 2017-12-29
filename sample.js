e9a21e7816169702e26484a424169409f980ae62
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-30 01:25:19 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -13,24 +13,17 @@ var schema = require('./schemas/schema');
 var app = express();
 app.use('*', cors({ origin: '*' }));
 
-
-// var server = http.createServer(function(request, response) {
-//
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello Express2");
-//
-// });
-
 var port = process.env.PORT || 1337;
 
-app.use('/graphql', (req, res) => {
-      res.writeHead(200, {"Content-Type": "text/plain"});
-      res.end("Hello Express4");
-})
+// app.use('/graphql', (req, res) => {
+//       res.writeHead(200, {"Content-Type": "text/plain"});
+//       res.end("Hello Express4");
+// })
 
-// server.listen(port);
-//
-// console.log("Server running at http://localhost:%d", port);
+app.use('/graphql',
+        bodyParser.json(),
+        graphqlHTTP({ schema: schema }));
+
 
 app.listen(port, () => {
     console.log('GraphQL Server is listening on: ' + port);
