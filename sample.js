a3293eccfd37f39cdafac9e2c04f2e7d3e0ee42d
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-30 01:09:34 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+var graphqlHTTP = require('express-graphql');
+
 var app = express();
 app.use('*', cors({ origin: '*' }));
 
