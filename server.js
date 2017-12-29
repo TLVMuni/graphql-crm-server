d3b07cbcaf94e2aebf83452a449abe70c0553780
Software Department of Tel-Aviv-Jaffo Municipality <34845633+TLVMuni@users.noreply.github.com>
2017-12-29 23:48:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
 }));
 
-const PORT = 80;
+const PORT = process.env.port || 3001;
 
 app.listen(PORT, () => {
     console.log(`GraphQL Server is listening on: ${PORT}`);
